e586aeeb600549148ee707bfc4cc5ed2abb4579d
magnatus <afonkinv@gmail.com>
2026-04-05 23:01:18 +0300

    Add templates and HTML

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Hello, vibe coding on Python 🚀</h1>
-    <p>Мое первое веб-приложение на Flask работает.</p>
-    """
-    
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
