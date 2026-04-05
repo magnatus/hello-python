f57fb638d99272aaf329f4e1dcfe1cece536f97c
magnatus <afonkinv@gmail.com>
2026-04-05 22:55:31 +0300

    Create first Flask app

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, vibe coding on Python 🚀")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello, vibe coding on Python 🚀</h1>
+    <p>Мое первое веб-приложение на Flask работает.</p>
+    """
+    
+if __name__ == "__main__":
+    app.run(debug=True)
